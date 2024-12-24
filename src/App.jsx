8f0e8725994fe62be4999ed75984e10785c60737
Anees <ayomideanees2003@icloud.com>
2024-12-24 09:21:29 +0100

    working with react and tailwind

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import ButtonGradient from "./assets/svg/ButtonGradient";
+
+const App = () => {
+  return (
+    //react fregmants <>;
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+      <ButtonGradient />
+    </>
+  );
+};
+
+export default App;
