556bbf78adb36e73b591fdc4085ac70e5f4800c4
Anees <ayomideanees2003@icloud.com>
2024-12-29 11:06:25 +0100

    working on section.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import { useLocation } from "react-router-dom";
-
+import { disablePageScroll, enablePageScroll } from "scroll-lock";
 import { brainwave } from "../assets";
 import { navigation } from "../constants";
 import Button from "./Button";
@@ -30,9 +30,10 @@ const Header = () => {
 
   return (
     <div
-      className={`fixed top-0 left-0 w-full z-50  border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ${
-        openNavigation ? "bg-n-8" : "bg-n-8/90 backdrop-blur-sm"
-      }`}
+      className={`fixed top-0 left-0 w-full z-50  border-b border-n-6
+       lg:bg-n-8/90 lg:backdrop-blur-sm ${
+         openNavigation ? "bg-n-8" : "bg-n-8/90 backdrop-blur-sm"
+       }`}
     >
       <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
         <a className="block w-[12rem] xl:mr-8" href="#hero">
